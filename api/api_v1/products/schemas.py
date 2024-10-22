16dac9cf2b1342d340fd89b0f3165db3bb3b7c42
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-22 07:48:30 +0300

    add Product schemas Product for crud

---
+++ api/api_v1/products/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class ProductBase(BaseModel):
+    name: str
+    description: str
+    price: int
+
+
+class Product(ProductBase):
+    # конвертирует бд модель в pydantic (json) модель для http ответа
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductUpdate(ProductBase):
+    pass
+
+
+class ProductUpdatePartial(ProductBase):
+    name: str | None = None
+    description: str | None = None
+    price: int | None = None
+
