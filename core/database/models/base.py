e164ea36b751acfe4603a84c141e30dc89ac5c31
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-22 17:46:24 +0300

    change autogeneration of name logic

--- core/database/models/base.py
+++ core/database/models/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 from core import settings
-from utils import camel_case_to_snake_case
+from utils import camel_case_to_snake_case, pluralize
 
 
 class Base(DeclarativeBase):
@@ -17,10 +17,10 @@ class Base(DeclarativeBase):
 
     # автоматическая генерация имени таблицы на основе название класса (CamelCase -> snake_case)
     # декоратор показывает что нужно сделать это в момент объявление класса, а
-    # не создания экземпляра
+    # не создания экземпляра и перевод во множественное число
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return f"{camel_case_to_snake_case(cls.__name__)}s"
+        return f"{pluralize(camel_case_to_snake_case(cls.__name__))}"
 
     # foo: Mapped[int]
     # bar: Mapped[int]
