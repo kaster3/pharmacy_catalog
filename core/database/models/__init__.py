790468825665942ccf6063f9fb0c995ed7411ab5
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-22 17:44:46 +0300

    move up Category, IntIdPkMixin, Tag class

--- core/database/models/__init__.py
+++ core/database/models/__init__.py
@@ -1,3 +1,11 @@
-__all__ = ("Product",)
+__all__ = (
+    "Product",
+    "Category",
+    "IntIdPkMixin",
+    "Tag",
+)
 
+from .categories import Category
+from .mixins import IntIdPkMixin
 from .products import Product
+from .tags import Tag
