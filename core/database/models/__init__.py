582e9d8b75cc9c0fa7f53f5b272ffea94fa6000c
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-22 18:35:35 +0300

    move up tag_product_association

--- core/database/models/__init__.py
+++ core/database/models/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "Category",
     "IntIdPkMixin",
     "Tag",
+    "association_table",
 )
 
 from .categories import Category
 from .mixins import IntIdPkMixin
 from .products import Product
 from .tags import Tag
+from .tag_product_association import association_table
