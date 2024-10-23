2741db820130e74d97b3e335364f8344f30e0e3d
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-23 13:15:48 +0300

    connet tag and product models

--- core/database/models/products.py
+++ core/database/models/products.py
@@ -5,9 +5,11 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 from .mixins import IntIdPkMixin
+from .tag_product_association import association_table
 
 if TYPE_CHECKING:
     from .categories import Category
+    from .tags import Tag
 
 
 class Product(Base, IntIdPkMixin):
@@ -21,4 +23,9 @@ class Product(Base, IntIdPkMixin):
     # Связываем 2 таблицы
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
     # Связываем 2 модели, чтобы обращаться с категории на ее продукты
-    categories: Mapped["Category"] = relationship(back_populates="products")
+    category: Mapped["Category"] = relationship(back_populates="products")
+    # Связываем 2 модели, чтобы с продуктов можно было обратиться на их теги
+    tags: Mapped[list["Tag"]] = relationship(
+        back_populates="products",
+        secondary=association_table,
+    )
